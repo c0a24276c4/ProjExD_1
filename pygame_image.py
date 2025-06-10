cce2427f87bbd16b39d280e499694e349142a07e
c0a24276c4 <c0a24276c4@edu.teu.ac.jp>
2025-06-10 15:49:49 +0900

    2枚目反転

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     kouka_img = pg.image.load("fig/3.png")
     kouka_img = pg.transform.flip(kouka_img,True,False)
     tmr = 0
@@ -18,7 +19,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img, [-tmr + 1600,0])  #2枚目
+        screen.blit(bg_img2, [-tmr + 1600,0])  #2枚目
         screen.blit(kouka_img,[300,200])
         pg.display.update()
         tmr += 1        
